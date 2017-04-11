9c8dd899ec2617ae8c8d217f27e38c5f0fcc1fe4
Sandhya Rani <nsandhyarani123@gmail.com>
2017-04-11 17:35:02 +0530

    updated json for product listing

--- app/scripts/controllers/productCtrl.js
+++ app/scripts/controllers/productCtrl.js
@@ -11,6 +11,8 @@ vtApp.controller('ProductCtrl',['$scope', '$route', '$log', function ($scope, $r
 	
 	$scope.healthCategoryFilter=["Health Category 1","Health Category 2","Health Category 3","Health Category 4","Health Category 5"];
 	
+	$scope.productList=[{"name":"My Product1", "image":"images/banner/1.jpg", "url":"", "description":"Product DEscription for the product djks djds dks jds"},{"name":"My Product1", "image":"images/banner/1.jpg", "url":"", "description":"Product DEscription for the product djks djds dks jds"},{"name":"My Product1", "image":"images/banner/1.jpg", "url":"", "description":"Product DEscription for the product djks djds dks jds"}];
+	
 	$scope.init = function() {
 		console.log(" Inside init of product Controller");
 	}
